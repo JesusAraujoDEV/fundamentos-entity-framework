b702f16609704ba3c5d7d49edc56a121786ea94d
jesusaraujodev <j2a0a0a5@gmail.com>
2025-05-20 14:34:30 -0400

    Clase 10: Add in-memory database support and update model validations
    - Integrated an in-memory database for TareasContext in Program.cs.
    - Added database connection endpoint to verify in-memory database status.
    - Enhanced Categoria model with data annotations for key and required fields.
    - Updated Tarea model with data annotations for key, foreign key, required fields, and added a NotMapped property for Resumen.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using fundamentos_entity_framework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContexto) => {
+    dbContexto.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContexto.Database.IsInMemory());
+});
 
 app.Run();
